e0f4270ee678d306f075e4d1470916db8a7a1950
NikhilNorth <ns4291@srmist.edu.in>
2026-03-11 09:45:10 +0530

    UC1: Add welcome message module

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class BookMyStayApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("Book My Stay App App");
+        System.out.println("Version : 1.0");
+        System.out.println("Welcome to Nikhil's Hotel and Room Finder");
+    }
+}
